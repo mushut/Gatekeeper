de37409b31a944b680c3faccfe8c3f6c480d67a8
Aki Sipovaara <mushut@gmail.com>
2018-08-30 14:15:26 +0300

    Koitin lisätä moduuleja. Kirjoitin toteutusta pidemmälle.

--- Gatekeeper.py
+++ Gatekeeper.py
@@ -1,6 +1,19 @@
 # Gatekeeper - Gateway application for sending data to IBM Cloud
 
+# Import modules!
+import RPi.GPIO as GPIO
+import spidev
+import ibmiotf.application
+
 def main():
     print("Gatekeeper")
+    spi = spidev.SpiDev()
+    spi.open(bus, device)
+
+    # SPI settings
+
+    test_send = [0x01, 0x02, 0x03]
+    spi.xfer(test_send)
+    # Or xfer2
 
 main()
