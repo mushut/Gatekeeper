8c3dd9a92029a0d9cad6add5e6530a46b02e9c31
Aki Sipovaara <mushut@gmail.com>
2018-08-31 11:56:16 +0300

    Vähän lisää hommaa

--- Gatekeeper.py
+++ Gatekeeper.py
@@ -1,17 +1,22 @@
 # Gatekeeper - Gateway application for sending data to IBM Cloud
 
-# Import modules!
-import RPi.GPIO as GPIO
+# Import modules! Needs to be run on raspberry pi to work.
+import RPi.GPIO as GPIO # Is this needed at all?
 import spidev
 import ibmiotf.application
 
 def main():
     print("Gatekeeper")
+
+    # Open SPI connection
+    # bus = ?
+    # device = ?
     spi = spidev.SpiDev()
     spi.open(bus, device)
 
     # SPI settings
 
+    # Send test data
     test_send = [0x01, 0x02, 0x03]
     spi.xfer(test_send)
     # Or xfer2
